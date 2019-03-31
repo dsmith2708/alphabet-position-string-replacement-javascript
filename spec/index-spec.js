c1d1b781035b6bbdb8463ad5f5aa433d079803f7
Daniel Smith <danielsmith.2796@googlemail.com>
2019-03-31 19:34:27 +0100

    both sanitiseString and getOutputString functions fully unit tested

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -1,6 +1,66 @@
 
 describe('sanitiseString function', () => {
-    
+
+    it('should return an already valid string argument unmodified', () => {
+        expect(sanitiseString('aaaa')).toEqual('aaaa');
+    })
+
+    it('should remove any whitespace from start of returned string', () => {
+        expect(sanitiseString(' bbbb')).toEqual('bbbb');
+    })
+
+    it('should remove any whitespace from middle of returned string', () => {
+        expect(sanitiseString('cc cc')).toEqual('cccc');
+    })
+
+    it('should remove any whitespace from end of returned string', () => {
+        expect(sanitiseString('dddd ')).toEqual('dddd');
+    })
+
+    it('should remove any symbols from the start of the string', () => {
+        expect(sanitiseString(';!"£eeee')).toEqual('eeee');
+    })
+
+    it('should remove any symbols from the middle of the string', () => {
+        expect(sanitiseString('ff!..;ff')).toEqual('ffff');
+    })
+
+    it('should remove any symbols from the end of the string', () => {
+        expect(sanitiseString('gggg%^&*')).toEqual('gggg');
+    })
+
+    it('should remove any numbers from the start of the string', () => {
+        expect(sanitiseString('1234hhhh')).toEqual('hhhh');
+    })
+
+    it('should remove any numbers from the middle of the string', () => {
+        expect(sanitiseString('ii5678ii')).toEqual('iiii');
+    })
+
+    it('should remove any numbers from the end of the string', () => {
+        expect(sanitiseString('jjjj91011')).toEqual('jjjj');
+    })
+
+    it('should change any uppercase letters into lowercase at the start of the string', () => {
+        expect(sanitiseString('Kkkk')).toEqual('kkkk');
+    })
+
+    it('should change any uppercase letters into lowercase at the start of the string', () => {
+        expect(sanitiseString('Kkkk')).toEqual('kkkk');
+    })
+
+    it('should change any uppercase letters into lowercase at the middle of the string', () => {
+        expect(sanitiseString('lLLl')).toEqual('llll');
+    })
+
+    it('should change any uppercase letters into lowercase at the end of the string', () => {
+        expect(sanitiseString('mmMM')).toEqual('mmmm');
+    })
+
+    it('should remove a mixture of whitespace, numbers, symbols and uppercase characters from a string to fully sanitise it', () => {
+        expect(sanitiseString('12$H%^0*e*509(()L6784l789£$%o')).toEqual('hello');
+    })
+
 })
 
 describe('getOutputString function', () => {
