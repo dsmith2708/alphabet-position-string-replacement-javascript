ca2d635c4fd6a7b115b405c6694e7f107190d706
Daniel Smith <danielsmith.2796@googlemail.com>
2019-03-31 13:58:57 +0100

    final output string now being created using charCodeAt to get position in alphabet

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ function convertToAlphabetIndex() {
     // Get string value from form
     let stringToConvert = document.getElementById('string-input').value;
 
-    //Sanitize string with regex and toLowerCase
+    // Sanitize string with regex and toLowerCase
     stringToConvert = stringToConvert.replace(/[^a-z]/gmi, "").replace(/\s+/g, "");
     stringToConvert = stringToConvert.toLowerCase();
+
+    // Construct output string using by gtting position in alphabet using charCodeAt
+    let convertedString = '';
+    [...stringToConvert].forEach((char) => {
+        console.log('position of ', char, ' in alphabet is: ', char.charCodeAt(0) - 96);
+        convertedString += (char.charCodeAt(0) - 96 + ' ');
+    });
+
+    console.log('final string: ', convertedString);
 }
