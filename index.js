1be7d2deac6b1c1751476099547d44b1e31580f2
Daniel Smith <danielsmith.2796@googlemail.com>
2019-03-31 13:29:19 +0100

    string also sanitised using toLowerCase

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function convertToAlphabetIndex() {
+    // Get string value from form
     let stringToConvert = document.getElementById('string-input').value;
+
+    //Sanitize string with regex and toLowerCase
     stringToConvert = stringToConvert.replace(/[^a-z]/gmi, "").replace(/\s+/g, "");
-    console.log('sanitized string: ', stringToConvert);
+    stringToConvert = stringToConvert.toLowerCase();
 }
